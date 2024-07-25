f9b8a0bd72ab26797f2c8fa3f2685abe884459e2
DianaFriptuleac <168530400+DianaFriptuleac@users.noreply.github.com>
2024-07-25 11:28:25 +0700

    last commit

--- script.js
+++ script.js
@@ -1,105 +1,162 @@
-const getLibrary = function() {
-    fetch(                                                             //ottengo i dati con fetch
+//fetch per ottenere i dati
+const getLibrary = function () {
+    fetch(
         'https://striveschool-api.herokuapp.com/books'
     )
-    .then((response) =>{
-       console.log(response)
-       if(response.ok){                                          //se la risposta e positiva converti in formato JSON
-        return response.json()                                   //se la risposta e negativa lancia il messaggio   
-       }else{
-        throw new Error('Qualcosa e andata storto con la chiamata in rete')
-       }
-    })
-    .then((listLibrary) =>{
-        console.log(listLibrary)
-
-     const bookContainer = document.getElementById('book-container')
-     listLibrary.forEach((book) => {                        
-      // creo la mia card con JS
-        const col = document.createElement('div')
-        col.classList.add('col')
-
-        const card = document.createElement ('div')
-        card.classList.add('card','h-100')
-
-        const img = document.createElement('img')
-        img.src = book.img
-        img.classList.add('card-img-top')
-        img.alt = book.title
-
-        const cardBody = document.createElement('div')
-        cardBody.classList.add('card-body')
-
-        const cardBodyh5 = document.createElement('h5')
-        cardBodyh5.classList.add('card-title')
-        cardBodyh5.innerText = book.title
-
-        const cardBookp = document.createElement('p')
-        cardBookp.classList.add('card-text')
-        cardBookp.innerText =  `Prezzo libbro: €${book.price}`
-        //creo i miei bottoni
-        const deleteButton = document.createElement('button')
-        deleteButton.classList.add('btn','btn-danger', 'me-4')
-        deleteButton.innerHTML = 'Scarta  <i class="bi bi-trash3"></i>'
-        deleteButton.addEventListener('click',()=>{
-            col.remove()
+        .then((response) => {
+            console.log(response)
+            //se la risposta e positiva converti in formato JSON
+            if (response.ok) {
+                return response.json()
+                //se la risposta e negativa lancia il messaggio                                
+            } else {
+                throw new Error('Qualcosa e andata storto con la chiamata in rete')
+            }
         })
-        const addButton = document.createElement('button')
-        addButton.classList.add('btn','btn-success')
-        addButton.innerHTML = 'Compra  <i class="bi bi-cart3"></i>'
-        addButton.addEventListener('click',()=>{
-            addBook(book)
-        })
-      // appendo ogni elemento della mia card
-        cardBody.appendChild(cardBodyh5)
-        cardBody.appendChild(cardBookp)
-        cardBody.appendChild(deleteButton)
-        cardBody.appendChild(addButton)
-        
-        card.appendChild(img)
-        card.appendChild(cardBody)
+        .then((listLibrary) => {
+            console.log(listLibrary)
+            //per ogni libro nell'array creo la mia card
+            const bookContainer = document.getElementById('book-container')
+            listLibrary.forEach((book) => {
 
-        col.appendChild(card)
-        bookContainer.appendChild(col)
-      
-    })
-})
-    .catch((err) =>{       //gestisco errori durante il fetch
-        console.log('Errore', err)
-    })
+                //il div col
+                const col = document.createElement('div')
+                col.classList.add('col')
+
+                //div card con altezza fissa di h-100
+                const card = document.createElement('div')
+                card.classList.add('card', 'h-100')
+
+                //img ddella card
+                const img = document.createElement('img')
+                img.src = book.img
+                img.classList.add('card-img-top')
+                img.alt = book.title
+
+                //div body-card
+                const cardBody = document.createElement('div')
+                cardBody.classList.add('card-body')
+
+                //h5 title-card e aggiungo un eventListener per espandere il titolo (in seguito faccio delle modifiche in CSS)
+                const cardBodyh5 = document.createElement('h5')
+                cardBodyh5.classList.add('card-title', 'title-overflow')
+                cardBodyh5.innerText = book.title
+                cardBodyh5.addEventListener('click', () => {
+                    cardBodyh5.classList.toggle('expanded-text')
+                })
+
+                //p con il prezzo del libro
+                const cardBookp = document.createElement('p')
+                cardBookp.classList.add('card-text')
+                cardBookp.innerText = `Prezzo libbro: €${book.price}`
+
+                //bottone per rimuovere card
+                const deleteButton = document.createElement('button')
+                deleteButton.classList.add('btn', 'btn-danger', 'me-4')
+                deleteButton.innerHTML = 'Scarta  <i class="bi bi-trash3"></i>'
+                deleteButton.addEventListener('click', () => {
+                    col.remove()
+                })
+
+                //bottone per aggiungere card alla lista
+                const addButton = document.createElement('button')
+                addButton.classList.add('btn', 'btn-success')
+                addButton.innerHTML = 'Compra  <i class="bi bi-cart3"></i>'
+                addButton.addEventListener('click', () => {
+                    addBook(book)
+                })
+                // appendo ogni elemento della mia card
+                cardBody.appendChild(cardBodyh5)
+                cardBody.appendChild(cardBookp)
+                cardBody.appendChild(deleteButton)
+                cardBody.appendChild(addButton)
+
+                card.appendChild(img)
+                card.appendChild(cardBody)
+
+                col.appendChild(card)
+                bookContainer.appendChild(col)
+
+            })
+        })
+        //gestisco errori durante la fetch e li visualizzo in console
+        .catch((err) => {
+            console.log('Errore', err)
+        })
 }
- //aggiundo un libro alla lista visibile nella pagina e salvo nel localStorage
-const addBook = (book) =>{
-    const bookList = document.getElementById('book-list')   //id della mia <ul>
-    const bookItem = JSON.parse(localStorage.getItem('bookItem')) || []   //recupero la lista dal localStorage e la converto in un array, ma se nn essiste mi dara un array vuoto
 
-    bookItem.push(book)   //agg il libro della funzione all array dei libri
-    localStorage.setItem('bookItem', JSON.stringify(bookItem))  //salvo l'array nel localStorage
+
+//aggiundo un libro alla lista visibile nella pagina e salvo nel localStorage
+const addBook = (book) => {
+    //id della mia <ul>
+    const bookList = document.getElementById('book-list')
+    //recupero la lista dal localStorage e la converto in un array, ma se nn essiste mi da un array vuoto
+    const bookItem = JSON.parse(localStorage.getItem('bookItem')) || []
+    //agg il libro della funzione all array dei libri
+    bookItem.push(book)
+    //salvo l'array nel localStorage 
+    localStorage.setItem('bookItem', JSON.stringify(bookItem))
 
     //creo la mia lista
     const newLi = document.createElement('li')
-    newLi.classList.add('list-group-item')   //classe bootstrap
-    newLi.innerText= book.title
+    //classe bootstrap
+    newLi.classList.add('list-group-item', 'd-flex', 'justify-content-between')
+    //titolo <li>
+    newLi.innerText = book.title
 
+
+
+    //button per rimuovere elemento dalla lista
+    const removeButton = document.createElement('button')
+    removeButton.classList.add('btn', 'btn-danger', 'ms-3')
+    removeButton.innerHTML = '<i class="bi bi-cart3"></i>'
+    removeButton.addEventListener('click', () => {
+        removeBook(book)
+    })
+    newLi.appendChild(removeButton)
     bookList.appendChild(newLi)  //appendo il <li> all mia <ul>
 }
 
+//funzione per rimuovere il libro dalla lista
+const removeBook = (book) => {
+    const bookList = document.getElementById('book-list')
+
+    //recupero la lista dal localStorage o creo un array vuoto
+    let bookItem = JSON.parse(localStorage.getItem('bookItem')) || []
+    //filtro l'array per rimuovere il libro con asin specifico
+    bookItem = bookItem.filter((item) => item.asin !== book.asin)
+    //salvo l'array aggiornata in localStorage
+    localStorage.setItem('bookItem', JSON.stringify(bookItem))
+    //chiamo la funzione per la lista aggiornata
+    loadingList()
+}
+
 //carico e visualizzo i libri salvati nel localStorage quando carico la pagina.
-const loadingList = () =>{
+const loadingList = () => {
     const bookList = document.getElementById('book-list')
     const bookItem = JSON.parse(localStorage.getItem('bookItem')) || []
+    //pulisco la lista
+    bookList.innerHTML = ''
+    // per ogni libbro nell array creo un elemento <li> e aggiungo il suo titolo 
+    bookItem.forEach((book) => {
+        const newLi = document.createElement('li')
+        newLi.classList.add('list-group-item', 'd-flex', 'justify-content-between')
+        newLi.innerHTML = book.title
 
-    bookList.innerHTML = ''  //pulisco la lista
+        const removeButton = document.createElement('button')
+        removeButton.classList.add('btn', 'btn-danger', 'ms-3')
+        removeButton.innerHTML = '<i class="bi bi-cart3"></i>'
+        removeButton.addEventListener('click', () => {
+            removeBook(book)
+        })
+        newLi.appendChild(removeButton)
 
-   bookItem.forEach((book) =>{                    // per ogni libbro nell array creo un elemento <li> e aggiungo il suo titolo 
-    const newLi = document.createElement('li')
-    newLi.classList.add('list-group-item')
-    newLi.innerHTML= book.title
-    bookList.appendChild(newLi)
-   })
+        bookList.appendChild(newLi)
+    })
 }
 //DomContentLoaded- evento che verifica se HTML e stato completamente caricato.
-document.addEventListener('DOMContentLoaded',() =>{    //eseuire la funzione loadingList() quando tutto l'HTML e carico. 
+//eseuire la funzione loadingList() quando tutto l'HTML e carico. 
+document.addEventListener('DOMContentLoaded', () => {
     loadingList()
 })
 
